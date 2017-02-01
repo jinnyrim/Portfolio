76e238cbf16eb123b596e6c90c946ebc608c269e
jinnyrim <Jinny@jinnys-imac.risd.edu>
2017-02-01 17:35:03 -0500

    fix!

--- js/4/sketch.js
+++ js/4/sketch.js
@@ -1,7 +1,7 @@
 var elephant;
 
 function setup() {
-  createCanvas(600,600);
+  createCanvas(windowWidth, windowHeight);
   elephant =loadImage("elephant.jpg");
 }
 
@@ -11,13 +11,13 @@ function draw() {
   imageMode(CENTER);
   image(elephant,random(200,400),random(200,400),elephant.width/7,elephant.height/7);
   fill(100,200,10,50);
-  rect(random(300),random(300),20,20);
+  rect(random(windowWidth/2),random(windowHeight/2),20,20);
   fill(200,100,10,80);
-  rect(random(300,600),random(300),30,30);
+  rect(random(windowWidth/2,windowWidth),random(windowHeight/2),30,30);
   fill(10,200,100,60);
-  rect(random(300),random(300,600),30,30);
+  rect(random(windowWidth/2),random(windowHeight/2,windowHeight),30,30);
   fill(100,10,200,70);
-  rect(random(300,600),random(300,600),20,20);
+  rect(random(windowWidth/2,windowWidth),random(windowHeight/2,windowHeight),20,20);
   fill(10,100,20,50)
   ellipse(mouseX,mouseY,80,80);
 }
